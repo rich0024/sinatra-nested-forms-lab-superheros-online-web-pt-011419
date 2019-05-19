9a09d202ca127ad2f6b4a784f20a7a1056ec2f14
Richard Yance <yance0022@gmail.com>
2019-05-19 00:59:00 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,16 +4,16 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
-  get '/' do 
+  get '/' do
      erb :super_hero
-  end 
-  
+  end
+
   post '/teams' do
     @team_name = params["team"]["name"]
     @team_motto = params["team"]["motto"]
     @team_members = params["team"]["members"]
-  
+
         erb :team
   end
-  
+
 end
